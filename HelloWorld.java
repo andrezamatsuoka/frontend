dd13538986ab3e9fc72e52db9025cf140495de55
andrezamatsuoka <120607732+andrezamatsuoka@users.noreply.github.com>
2024-08-13 10:13:43 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * public: declaração que "exporta" e permite que a classe/método/variável seja importada/reutilizada em outras classes/objetos
+ * clss: declaração de classe (que poderá se tornar um objeto)
+ * HelloWorld: nome da classe, seguindo o padrão de nomenclatura PascalCase
+ * {("abre" chave): sere para declarar o início de um bloco de códigos/ações
+ * }("fecha" chave): serve para declarar o fim de um bloco de códigos/ações
+ */
+ public class HelloWorld { // Aqui é criada a classe HelloWorld
+    /**
+     * static: declaração de método somente leitura, o qual não poderá ser editado, exceto pelo uso de @Override
+     * void: modo protegido de um método, o qual não retornará qualquer valor
+     * main: nome do método "executor" 
+     * ((abre parênteses): serve para declarar o início das declarações de parâmetros de um método/função
+     * )(fecha parênteses): serve para declarar o fim das declarações de parâmetros de um método/função
+     * @param args
+     * system: invoca a classe de sistema
+     * . (ponto): serve como operador de invocação de método
+     * out: invoca o método, ou função abstrata, o qual irá "imprimir" na tela uma string
+     * "Hello World": testo (string) que será "impresso" na tela
+     * ; (ponto e vírgula): serve para encerrar uma linha de código
+     */
+    public static void main(String[] args) { // Aqui é criado o método executor main 
+        System.out.println(x:"Hello World!!!"); //Aqui é "impressa" uma linha com o texto "Hello World" 
+    } // Aqui encerra o bloco de código do método main
+ } // Aqui encerra o bloco de código da classe HelloWorld
+
+ 
