af4cdd918e4c99d7b21502d4b992dcba3caad4c6
andrezamatsuoka <120607732+andrezamatsuoka@users.noreply.github.com>
2024-08-15 09:16:37 -0300

    tradução de texto para o usuário

--- Addition.java
+++ Addition.java
@@ -5,9 +5,9 @@ public class Addition // Cria/exporta/declara a classe (começa com letra maiús
    public static void main(String[] args) // Declara o método main, sem retorno (void), somente leitura (static), exportado nos parâmetros(args) do tipo matriz de string
    {
       String firstNumber = // declaração de variável (colocar o tipo, depois o nome da variável) e atribui-se um valor a ela (=)
-         JOptionPane.showInputDialog("Enter first integer"); //  o componente (JOptionPane) invoca (.) o método (showInputDialog) - o que está entre "texto", será impresso na tela
+         JOptionPane.showInputDialog("Digite o primeiro número"); //  o componente (JOptionPane) invoca (.) o método (showInputDialog) - o que está entre "texto", será impresso na tela
       String secondNumber =
-         JOptionPane.showInputDialog("Enter second integer");
+         JOptionPane.showInputDialog("Digite o segundo número");
 
       int number1 = Integer.parseInt(firstNumber); //declaração de variável (tipo, nome), atribui-se valor a ela (=). o método (parseInt - analisa número inteiro) invoca (.) o componente (Integer) que converte o parâmetro ()
       int number2 = Integer.parseInt(secondNumber);
@@ -15,8 +15,8 @@ public class Addition // Cria/exporta/declara a classe (começa com letra maiús
       int sum = number1 + number2; // declaração de variável que vai receber a soma dos números 
 
       
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, 
-         "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE); 
+      JOptionPane.showMessageDialog(null, "A soma dos números é:" + sum, 
+         "Soma de dois inteiros", JOptionPane.PLAIN_MESSAGE); 
          // o componente (JOptionPane) invoca (.) o método (showInputDialog). 
          //"The sum is " é uma string, e qualquer coisa que concatenar(+) com ela (sum), automaticamente já será convertida em string
          // (PLAIN_MESSAGE) - deixa a mensagem plana - está em scream case, somente para leitura e não pode ser alterada
